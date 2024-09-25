5fbc60d3c0045f5598c6c98fd78e05367117a255
Lenka <komet8ka@gmail.com>
2024-09-25 19:34:36 +0200

    feat: vytvořeny modely, založen další uživatel

--- Events/urls.py
+++ Events/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from viewer.models import Event
+
+admin.site.register(Event)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path('main_page/', main)
--- viewer/models.py
+++ viewer/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from django.db.models import (
+  DO_NOTHING, CharField, DateField, DateTimeField, ForeignKey, IntegerField,
+  Model, TextField, BooleanField, ManyToManyField
+)
 
-# Create your models here.
+class EventType(Model):
+  name = CharField(max_length=128)
+
+class Event(Model):
+  name = CharField(max_length=128)
+  # date = DateField
+  # place = CharField
+  # time = DateTimeField
+  # eventType = ManyToManyField(EventType, on_delete=DO_NOTHING)
+  # entry = BooleanField
+
+
+

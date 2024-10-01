ccd676bf9e035ea6eff119911a056b578a28ca3f
hanakantorova <kantorova.hanna@gmail.com>
2024-10-01 17:59:22 +0200

    feat:úprava modelu a nahrání eventů/event typů

--- viewer/models.py
+++ viewer/models.py
@@ -1,25 +1,27 @@
 import datetime
+from django.utils import timezone
+
 
 from django.db.models import (
-  DO_NOTHING, CharField, DateField, DateTimeField, ForeignKey, IntegerField,
-  Model, TextField, BooleanField, ManyToManyField
+    DO_NOTHING, CharField, DateField, DateTimeField, ForeignKey, IntegerField,
+    Model, TextField, BooleanField, ManyToManyField
 )
 
+
 class EventType(Model):
-  name = CharField(max_length=128)
+    name = CharField(max_length=128)
+
 
 class Event(Model):
-  name = CharField(max_length=128)
-  # date = DateField
-  # place = CharField
-  # time = DateTimeField
-  # eventType = ManyToManyField(EventType, on_delete=DO_NOTHING)
-  # entry = BooleanField
+    name = CharField(max_length=128)
+    #date = DateField(default=timezone.now)
+    place = CharField(max_length=128)
+    entry = BooleanField(default=True)
+    #eventType = ManyToManyField(EventType, on_delete=DO_NOTHING)
+
 
 class Comment(Model):
-  uzivatel = CharField(max_length=128, default="")
-  comment = TextField(default="")
-  event = ForeignKey(Event, on_delete=DO_NOTHING, default="")
-  comment_date = DateTimeField(default=datetime.datetime.now)
-
-
+    uzivatel = CharField(max_length=128, default="")
+    comment = TextField(default="")
+    event = ForeignKey(Event, on_delete=DO_NOTHING, default="")
+    comment_date = DateTimeField(default=datetime.datetime.now)

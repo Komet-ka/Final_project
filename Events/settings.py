93f7ffec062093005765ca40bac3b47cfdcdb77e
hanakantorova <kantorova.hanna@gmail.com>
2024-10-12 20:49:45 +0200

    feat: podmínky a hlášky pro změnu hesla a osobních údajů - nastaveno na cs

--- Events/settings.py
+++ Events/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'Events.urls'
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'cs'
 
 TIME_ZONE = 'Europe/Prague'
 

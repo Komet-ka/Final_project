1bfb777f80dbdccdb83dadc292dfe032d26fc9e0
hanakantorova <kantorova.hanna@gmail.com>
2024-10-13 17:27:59 +0200

    feat: přesunutí dlaždic eventů také na hlavní úvodní stránku

--- viewer/views.py
+++ viewer/views.py
@@ -237,8 +237,8 @@ def main_page(request):
     request,
     "main_page.html",
     context={
-          "newest_events": Event.objects.order_by("-create_date").all()[:5],
-          "nearest_events": Event.objects.order_by("-date").all()[:5],
+          "newest_events": Event.objects.order_by("-create_date").all()[:4],
+          "nearest_events": Event.objects.order_by("-date").all()[:4],
           "newest_comments": Comment.objects.order_by("-comment_date").all()[:5],
     }
   )

1bb01b62d67d369f98b28890a0ab6f82e0fe977f
hanakantorova <kantorova.hanna@gmail.com>
2024-10-19 22:31:26 +0200

    feat: doplnění lokálního resetu hesla

--- Events/urls.py
+++ Events/urls.py
@@ -20,6 +20,7 @@ from django.urls import path
 
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
 
 from viewer.views import detail, my_page, main_page, logout_view, attendees, search_view, api_upcoming_events, \
     list_events, delete_comment, SendEmailToAllView, SendEmailToAttendeeView
@@ -72,7 +73,10 @@ urlpatterns = [
     path('api/get/all_events/', api_upcoming_events, name='api_upcoming_events'),
     path('list_events/', list_events, name='list_events'), #tohle je v jiné aplikaci
 
-
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 
 if settings.DEBUG:

ac1a8db649ac490a6bea406717f8ec31de33a3fe
hanakantorova <kantorova.hanna@gmail.com>
2024-10-19 16:20:16 +0200

    feat: nahrávání dat do dtb, drobné úpravy ve stylu

--- viewer/models.py
+++ viewer/models.py
@@ -1,10 +1,5 @@
-import datetime
-
-from django.forms import ImageField
 from django.utils import timezone
-
 from django.db import models
-
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -48,9 +43,9 @@ class Event(Model):
 
 
 class Comment(Model):
-  comment = TextField(default="")
+  comment = CharField(max_length=500)
   event = ForeignKey(Event, on_delete=models.CASCADE, default="")
-  comment_date = DateTimeField(default=datetime.datetime.now)
+  comment_date = models.DateTimeField(default=timezone.now())
   user = ForeignKey(User, on_delete=models.CASCADE, default=1)
 
 
